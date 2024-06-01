1d3bdfe60900003d0fa58e742deb73bcd82bf279
benoy_Judy <benoyjudy@gmail.com>
2024-06-01 16:48:18 +0530

    frontpage map setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
-
+import React from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: 76.2673,
+          latitude: 9.9312,
+          zoom: 6
+        }}
+        style={{ width: '100VW', height: '100VH' }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
     </div>
   );
 }
