45e64cde4b5f9017b749f87ae6910484c0e957fd
benoy_Judy <benoyjudy@gmail.com>
2024-06-01 22:58:39 +0530

    popup and marker set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import Map from 'react-map-gl';
+import React, { useState } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    longitude: 76.8671,
+    latitude: 8.8607,
+    zoom: 10
+  });
+  const [showPopup, setShowPopup] = useState(true);
+
+
+
+  const handleViewportChange = (newViewport) => {
+    setViewport(newViewport);
+  }
+
   return (
     <div className="App">
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        initialViewState={{
-          longitude: 76.2673,
-          latitude: 9.9312,
-          zoom: 6
-        }}
+        initialViewState={viewport}
+        onViewportChange={handleViewportChange}
         style={{ width: '100VW', height: '100VH' }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
+      > <Marker longitude={76.8671} latitude={8.8607} anchor="bottom" >
+          <LocationOnIcon
+            style={{ fontSize: `${viewport.zoom * 7}px` }}
+            className=" text-blue-600"
+          />
+        </Marker>
+        {showPopup && (
+          <Popup longitude={76.8671} latitude={8.8607}
+            anchor="top"
+            onClose={() => setShowPopup(false)}>
+            You are here
+          </Popup>)}
+      </Map>
     </div>
   );
 }
