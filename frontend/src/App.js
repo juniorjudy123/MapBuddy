8324e31a7424b3fdba0319cb1b8e306570fd19a3
benoy_Judy <benoyjudy@gmail.com>
2024-06-04 14:47:29 +0530

    markerIcon clicks -popups management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import PopupCardComponent from './components/PopupCardComponent';
+import axios from 'axios'
+
 
 function App() {
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null)
   const [viewport, setViewport] = useState({
     longitude: 76.8671,
     latitude: 8.8607,
-    zoom: 10
+    zoom: 4
   });
   const [showPopup, setShowPopup] = useState(true);
+  // Function to handle marker clicks
+
+  const handleLocationClick = (id) => {
+    console.log('clicked marker id: ' + id);
+    setCurrentPlaceId(id);
+
+  }
+  // Fetch marker data on component mount
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/api/pins");
+        console.log("fetched marker data:", res.data);
+        setPins(res.data);
+      } catch (error) {
+        console.log('Error fetching marker data:', error)
+      }
+    }
+    getPins();
+  }, []);
 
 
 
@@ -26,21 +51,36 @@ function App() {
         onViewportChange={handleViewportChange}
         style={{ width: '100VW', height: '100VH' }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      > <Marker longitude={76.8671} latitude={8.8607} anchor="bottom" >
-          <LocationOnIcon
-            style={{ fontSize: `${viewport.zoom * 7}px` }}
-            className=" text-blue-600"
-          />
-        </Marker>
-        {showPopup && (
-          <Popup longitude={76.8671} latitude={8.8607}
-            anchor="top"
-          >
-            <PopupCardComponent />
+      >
 
-          </Popup>)}
+        {pins.map(p => (
+          < div key={p._id}>
+            <Marker longitude={p.longitude} latitude={p.latitude} anchor="bottom" >
+
+              <LocationOnIcon
+                style={{ fontSize: '24px' }}
+                className="text-blue-600 cursor-pointer"
+                onClick={() => handleLocationClick(p._id)}
+              />
+
+            </Marker>
+
+
+            {p._id === currentPlaceId && (
+              <Popup
+                longitude={p.longitude}
+                latitude={p.latitude}
+                anchor="top"
+                onClose={() => setCurrentPlaceId(null)}
+                closeOnClick={false}
+              >
+                <PopupCardComponent pin={p} />
+              </Popup>
+            )})
+          </div>))}
+
       </Map>
-    </div>
+    </div >
   );
 }
 
--- frontend/src/components/PopupCardComponent.jsx
+++ frontend/src/components/PopupCardComponent.jsx
@@ -1,19 +1,19 @@
 import React from "react"
 import StarIcon from "@mui/icons-material/Star"
 import QueryBuilderIcon from "@mui/icons-material/QueryBuilder"
-import { colors } from "@mui/material"
+import { format } from "timeago.js"
 
-function PopupCardComponent() {
+const PopupCardComponent = ({ pin }) => {
 	return (
 		<div className="  w-52 h-52 flex flex-col justify-around">
 			<label className="text-tomato font-light  border-b-0.5 border-tomato  w-fit h-fit ">
 				Place
 			</label>
-			<h4 className="font-bold">Eiffel Tower</h4>
+			<h4 className=" font-semibold text-lg">{pin.title}</h4>
 			<label className="text-tomato font-light  border-b-0.5 border-tomato  w-fit h-fit ">
 				Review
 			</label>
-			<p>Beautiful Place.I like it</p>
+			<p className="  font-serif text-base">{pin.description}</p>
 			<label className="text-tomato font-light  border-b-0.5 border-tomato  w-fit h-fit ">
 				Rating
 			</label>
@@ -27,12 +27,12 @@ function PopupCardComponent() {
 			<label className="text-tomato font-light  border-b-0.5 border-tomato  w-fit h-fit ">
 				Information
 			</label>
-			<span className="">
-				created by <span className="font-bold">Benoy</span>
+			<span>
+				created by <span className="font-bold">{pin.username}</span>
 			</span>
 			<div className="flex items-center ">
-				<QueryBuilderIcon class="w-4 h-4 mr-1" />
-				<span className="font-thin">1 hour ago</span>
+				<QueryBuilderIcon className="w-4 h-4 mr-1 " />
+				<span className="font-thin text-gray-500">{format(pin.createdAt)}</span>
 			</div>
 		</div>
 	)
